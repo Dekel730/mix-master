5fb2dccf96b4ddea32e9b5649465dad13d8f664c
Omer Gaiznger <Omerg863@gmail.com>
2025-01-24 12:26:21 +0200

    mongo connection fix

--- server/config/db.ts
+++ server/config/db.ts
@@ -3,14 +3,19 @@ import colors from 'colors';
 
 const connectDB = async (listen: () => void): Promise<void> => {
 	try {
-		const connectionUri =
-			process.env.NODE_ENV === 'test'
-				? process.env.MONGO_URI_TEST!
-				: process.env.MONGO_URI!;
-		const conn = await mongoose.connect(connectionUri, {
-			user: process.env.MONGO_USERNAME,
-			pass: process.env.MONGO_PASSWORD,
-		});
+		let conn;
+		if (process.env.NODE_ENV === 'development') {
+			conn = await mongoose.connect(process.env.MONGO_URI!, {
+				user: process.env.MONGO_USERNAME,
+				pass: process.env.MONGO_PASSWORD,
+			});
+		} else {
+			const connectionUri =
+				process.env.NODE_ENV === 'test'
+					? process.env.MONGO_URI_TEST!
+					: process.env.MONGO_URI!;
+			conn = await mongoose.connect(connectionUri);
+		}
 		console.log(
 			colors.cyan.underline(`MongoDB Connected: ${conn.connection.host}`)
 		);
