19ddd9729748106f2f9f8a5480a05426cde91cfb
Omer Gaiznger <Omerg863@gmail.com>
2025-01-25 13:19:15 +0200

    verify page change

--- client/src/pages/Verify.tsx
+++ client/src/pages/Verify.tsx
@@ -39,14 +39,21 @@ const Verify = () => {
 
 	return (
 		<main>
-			<RedTitle title="Verifying Account..." />
-			{isLoading && <Spinner width="w-24" height="h-24" />}
-			{success && (
-				<span>
-					You will be redirected to the login page in a few second if
-					not click <Link to={'/login'}>here</Link>
-				</span>
-			)}
+			<div className="min-h-screen bg-[#1a1a1a] flex items-center justify-center p-4">
+				<div className="w-full max-w-md relative">
+					<div className="bg-[#212121] rounded-3xl p-8 md:p-12 shadow-xl relative z-10">
+						<RedTitle title="Verifying Account..." />
+						{isLoading && <Spinner width="w-24" height="h-24" />}
+						{success && (
+							<span>
+								You will be redirected to the login page in a
+								few second if not click{' '}
+								<Link to={'/login'}>here</Link>
+							</span>
+						)}
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 };
